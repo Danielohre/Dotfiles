9c05159a5007c1f9d8f2a777ce000136135ebb40
Daohre <daniel.ohresten@hotmail.com>
2024-04-22 09:10:06 +0200

    Terminal will now recalculate its size on open

--- nvim/lua/plugins/toggle-terminal/init.lua
+++ nvim/lua/plugins/toggle-terminal/init.lua
@@ -1,10 +1,20 @@
 local M = {}
 
-local terminalWindow 
+local terminalWindow
 local terminalBuffer
 local window_opts
+local width_scale
+local height_scale
+
 function M.toggle()
 	if vim.fn.getbufinfo(terminalBuffer)[1].hidden == 1 then
+		local ui = vim.api.nvim_list_uis()[1]
+		local width = math.floor((ui.width * width_scale) + 0.5)
+		local height = math.floor((ui.height * height_scale) + 0.5)
+		window_opts.width = width
+		window_opts.height = height
+		window_opts.col = (ui.width/2) - (width/2)
+		window_opts.row = (ui.height/2) - (height/2)
 		terminalWindow = vim.api.nvim_open_win(terminalBuffer, 1, window_opts)
 	else
 		vim.api.nvim_win_hide(terminalWindow)
@@ -34,15 +44,10 @@ function M.setup(opts)
 		vim.cmd('terminal')
 	end
 	vim.cmd('highlight TerminalTitle guifg=#0dbdba')
-	local ui = vim.api.nvim_list_uis()[1]
-	local width = math.floor((ui.width * opts.width_scale) + 0.5) 
-	local height = math.floor((ui.height * opts.height_scale) + 0.5)
+	width_scale = opts.width_scale
+	height_scale = opts.height_scale
 	window_opts = {
 		relative = 'editor',
-		width = width,
-		height = height,
-		col = (ui.width/2) - (width/2),
-		row = (ui.height/2) - (height/2),
 		anchor = 'NW',
 		style = 'minimal',
 		border = 'single',
@@ -50,11 +55,11 @@ function M.setup(opts)
 		title_pos = 'center'
 	}
 	terminalBuffer = vim.api.nvim_create_buf(true, false)
-	terminalWindow = vim.api.nvim_open_win(terminalBuffer, 1, window_opts)
+	M.toggle()
 	vim.api.nvim_win_set_option(terminalWindow, 'winhighlight', 'Normal:TerminalNormal')
 	vim.api.nvim_win_call(terminalWindow, TerminalMode)
 	vim.api.nvim_win_hide(terminalWindow)
-	vim.api.nvim_create_user_command('ToggleTerminal',  M.toggle, {bang= true})
+	vim.api.nvim_create_user_command('ToggleTerminal',  M.toggle, {bang = true})
 end
 
 return M
