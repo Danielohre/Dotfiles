104b3f066092b0ae88fd9e26eeadae7053b6fcdc
Daohre <daniel.ohresten@hotmail.com>
2024-04-26 10:10:53 +0200

    better name for setup script, script now creates symlinks instead of moving, script also symlinks .tmux.conf

--- dotfiles_setup.sh
+++ dotfiles_setup.sh
@@ -12,6 +12,7 @@ sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.
        https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
 
 
-sudo mv nvim/ ~/.config/
+sudo ln -s $(dirname $0)/nvim ~/.config/
+sudo ln -s $(dirname $0)/.tmux.conf ~/
 
 echo Open NVIM and run :PlugInstall
