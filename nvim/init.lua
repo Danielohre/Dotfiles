75549daf7e6b3bedc2014e2e44374159d6e3ab60
danielohre <daniel.ohresten@hotmail.com>
2025-08-22 21:01:59 +0200

    enabled lua_ls lsp

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,27 +1,28 @@
 require('config.options')
+
 require('config.mappings')
+
 require('plugins.vim-plug')
+
 vim.cmd 'set termguicolors'
 vim.cmd 'colorscheme nightfly'
-require('plugins.put').setup({width_scale = 0.8, height_scale = 0.8})
-require('plugins.bpp').setup(
-{
-	style = {title='#805514' ,list='#048c12' ,end_of_buffer='bg'},
-	instructions_data = {
-		'<leader>m      : Toggle Terminal',
-		'<leader>ff : Find File in CWD',
-		'<leader>fg : LiveGrep In CWD',
-		'<leader>fb : Buffer Search',
-		'<leader>q  : Open File Tree'
-	},
-	recents_data = {}
 
-}
+require('plugins.put').setup({ width_scale = 0.8, height_scale = 0.8 })
+
+require('plugins.bpp').setup(
+	{
+		style = { title = '#805514', list = '#048c12', end_of_buffer = 'bg' },
+		instructions_data = {
+			'<leader>m      : Toggle Terminal',
+			'<leader>ff : Find File in CWD',
+			'<leader>fg : LiveGrep In CWD',
+			'<leader>fb : Buffer Search',
+			'<leader>q  : Open File Tree'
+		},
+		recents_data = {}
+
+	}
 )
 
 require('config.autocommands')
 require('config.lsp.lspConfig')
-
-
-
-
--- nvim/lua/config/lsp/lspConfig.lua
+++ nvim/lua/config/lsp/lspConfig.lua
@@ -1,10 +1,42 @@
+vim.lsp.config.lua_ls = {
+	-- Command and arguments to start the server.
+	cmd = { 'lua-language-server' },
+
+	-- Filetypes to automatically attach to.
+	filetypes = { 'lua' },
+
+	-- Sets the "root directory" to the parent directory of the file in the
+	-- current buffer that contains either a ".luarc.json" or a
+	-- ".luarc.jsonc" file. Files that share a root directory will reuse
+	-- the connection to the same LSP server.
+	-- Nested lists indicate equal priority, see |vim.lsp.Config|.
+	root_markers = { { '.luarc.json', '.luarc.jsonc' }, '.git' },
+
+	-- Specific settings to send to the server. The schema for this is
+	-- defined by the server. For example the schema for lua-language-server
+	-- can be found here https://raw.githubusercontent.com/LuaLS/vscode-lua/master/setting/schema.json
+	settings = {
+		Lua = {
+			runtime = {
+
+				version = 'LuaJIT',
+			},
+			diagnostics = {
+				-- Get the language server to recognize the `vim` global
+				globals = { 'vim' },
+			},
+		}
+	}
+}
+vim.lsp.enable('lua_ls')
+
 
 vim.lsp.config.clangd = {
-	cmd = { 'clangd', "--log=verbose"},
-	filetypes = {'c', 'cpp', 'h' },
+	cmd = { 'clangd', "--log=verbose" },
+	filetypes = { 'c', 'cpp', 'h' },
 	capabilities = require('blink.cmp').get_lsp_capabilities(),
 	root_markers = { '.clangd', '.clang-tidy', '.clang-format', 'compile_commands.json', 'compile_flags.txt',
-						'configure.ac', '.git'}
+		'configure.ac', '.git' }
 }
 vim.lsp.enable('clangd')
 
